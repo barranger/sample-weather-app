ee83fc54ea0ad9e13ef974f90f6adc7842b46d02
Barranger Ridler <v-bridler@redrocktechsoltuions.com>
2020-08-16 13:45:12 -0400

    Initial Commit

--- App.js
+++ App.js
@@ -1,21 +1,157 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, ActivityIndicator, SafeAreaView, ScrollView, FlatList } from 'react-native';
 
-export default function App() {
+const openWeatherKey = '';
+const url = `https://api.openweathermap.org/data/2.5/onecall?lat=43.379100&lon=-79.757400&units=metric&exclude=minutely&appid=${openWeatherKey}`;
+
+const App = () => {
+
+  const [forcast, setForcast] = useState(null);
+
+  useEffect(() => {
+    const loadForcast = async () => {
+      if (!forcast) {
+        var response = await fetch(url);
+        var data = await response.json();
+        setForcast(data);
+      }
+    }
+    loadForcast();
+  })
+
+  if (!forcast) {
+    return <SafeAreaView style={styles.loading}>
+      <ActivityIndicator size="large" />
+      </SafeAreaView>;
+  }
+
+  const current = forcast.current.weather[0];
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <Text style={styles.title}>Current Weather</Text>
+        <View style={styles.current}>
+          <Image
+            style={styles.largeIcon}
+            source={{
+              uri: `http://openweathermap.org/img/wn/${current.icon}@4x.png`,
+            }}
+          />
+          <Text style={styles.currentTemp}>{Math.round(forcast.current.temp)}°C</Text>
+        </View>
+        
+        <Text style={styles.currentDescription}>{current.description}</Text>
+        <View>
+          <Text style={styles.subtitle}>Hourly Forcast</Text>
+          <FlatList horizontal
+            data={forcast.hourly.slice(0, 24)}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={(hour) => {
+              const weather = hour.item.weather[0];
+              var dt = new Date(hour.item.dt * 1000);
+              return <View style={styles.hour}>
+                <Text>{dt.toLocaleTimeString().replace(/:\d+ /, ' ')}</Text>
+                <Text>{Math.round(hour.item.temp)}°C</Text>
+                <Image
+                  style={styles.smallIcon}
+                  source={{
+                    uri: `http://openweathermap.org/img/wn/${weather.icon}@4x.png`,
+                  }}
+                />
+                <Text>{weather.description}</Text>
+              </View>
+            }}
+          />
+        </View>
+
+        <Text style={styles.subtitle}>Next 5 Days</Text>
+        {forcast.daily.slice(0,5).map(d => { //Only want the next 5 days
+          const weather = d.weather[0];
+          var dt = new Date(d.dt * 1000);
+          return <View style={styles.day} key={d.dt}>
+            <Text style={styles.dayTemp}>{Math.round(d.temp.max)}°C</Text>
+            <Image
+              style={styles.smallIcon}
+              source={{
+                uri: `http://openweathermap.org/img/wn/${weather.icon}@4x.png`,
+              }}
+            />
+            <View style={styles.dayDetails}>
+              <Text>{dt.toLocaleDateString()}</Text>
+              <Text>{weather.description}</Text>
+            </View>
+          </View>
+        })}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  title: {
+    width: '100%',
+    textAlign: 'center',
+    fontSize: 42,
+    color: '#e96e50',
+  },
+  subtitle: {
+    fontSize: 24,
+    marginVertical: 12,
+    marginLeft: 4,
+    color: '#e96e50',
+  },
   container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  loading: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  current: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+  currentTemp: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },  
+  currentDescription: {
+    width: '100%',
+    textAlign: 'center',
+    fontWeight: '200',
+    fontSize: 24,
+    marginBottom: 24
+  },
+  hour: {
+    padding: 6,
+    alignItems: 'center',
+  },
+  day: {
+    flexDirection: 'row',
+  },
+  dayDetails: {
+    justifyContent: 'center',
+  },
+  dayTemp: {
+    marginLeft: 12,
+    alignSelf: 'center',
+    fontSize: 20
+  },
+  largeIcon: {
+    width: 250,
+    height: 200,
+  },
+  smallIcon: {
+    width: 100,
+    height: 100,
+  }
 });
+
+export default App;
---
+++ README.md
@@ -0,0 +1,14 @@
+# React Native Weather Sample App
+
+This is a sample application to go along with the blog post [found here](https://redbitdev.com/blog)
+
+## How to run this app
+
+To run the app, simply clone the repo, edit `App.js` line 4 to your own API key and run the following commands:
+
+```
+npm install
+npm run ios
+```
+
+and it should pop up in the ios Simulator.
